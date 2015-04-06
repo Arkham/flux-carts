cdbfd5e7be7a19c2694cc425c221340d0dbb9871
Ju Liu <ju.liu@welaika.com>
2015-04-06 01:27:18 +0100

    First steps in flux

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flux carts
+
+Using different implementations of flux to implement a checkout cart.
+
+## Facebook Flux
---
+++ flux/js/actions/FluxCartActions.js
@@ -0,0 +1,40 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var FluxCartConstants = require('../constants/FluxCartConstants');
+
+module.exports = {
+  receiveProduct: function(data) {
+    AppDispatcher.handleAction({
+      actionType: FluxCartConstants.RECEIVE_DATA,
+      data: data
+    });
+  },
+
+  selectProduct: function(index) {
+    AppDispatcher.handleAction({
+      actionType: FluxCartConstants.SELECT_PRODUCT,
+      data: index
+    });
+  },
+
+  addToCart: function(sku, update) {
+    AppDispatcher.handleAction({
+      actionType: FluxCartConstants.CART_ADD,
+      sku: sku,
+      update: update
+    });
+  },
+
+  removeFromCart: function(sku) {
+    AppDispatcher.handleAction({
+      actionType: FluxCartConstants.CART_REMOVE,
+      sku: sku
+    });
+  },
+
+  updateCartVisible: function(cartVisible) {
+    AppDispatcher.handleAction({
+      actionType: FluxCartConstants.CART_VISIBLE,
+      cartVisible: cartVisible
+    });
+  }
+};
